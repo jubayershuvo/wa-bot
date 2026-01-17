104f18acb6adf45dc54d1f1402d3c9947f466ce7
Md Jubayer <mdjubayerislamshuvo34@gmail.com>
2026-01-18 00:00:53 +0600

    sadas

---
+++ app/file/[id]/route.ts
@@ -0,0 +1,64 @@
+import { connectDB } from "@/lib/mongodb-bot";
+import PostFile from "@/models/Order";
+import { NextResponse } from "next/server";
+import fs from "fs";
+import path from "path";
+
+export async function GET(
+  req: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    await connectDB();
+    const { id } = await params;
+
+    if (!id) {
+      return NextResponse.json({ error: "Id not found" }, { status: 404 });
+    }
+
+    // 1. Fetch record
+    const record = await PostFile.findById(id);
+    if (!record) {
+      return NextResponse.json(
+        { error: "File record not found" },
+        { status: 404 }
+      );
+    }
+
+    const filePath = record.deliveryData?.fileUrl;
+
+    if (!filePath) {
+      return NextResponse.json(
+        { error: "File path not found" },
+        { status: 404 }
+      );
+    }
+
+    // 2. Check file exists
+    if (!fs.existsSync(filePath)) {
+      return NextResponse.json(
+        { error: "File not found on server" },
+        { status: 404 }
+      );
+    }
+
+    // 3. Extract filename & extension from local file
+    const parsed = path.parse(filePath);
+    const safeFileName = parsed.name.replace(/[^a-zA-Z0-9]/g, "_");
+    const finalFileName = `${safeFileName}${parsed.ext}`;
+
+    // 4. Read file
+    const fileBuffer = fs.readFileSync(filePath);
+
+    // 5. Headers
+    const headers = new Headers({
+      "Content-Type": "application/octet-stream",
+      "Content-Disposition": `attachment; filename="${finalFileName}"`,
+    });
+
+    return new Response(fileBuffer, { headers });
+  } catch (error) {
+    console.error("File download error:", error);
+    return NextResponse.json({ error: "Server error" }, { status: 500 });
+  }
+}
